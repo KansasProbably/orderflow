c1feac36382a1cd9d887caf5341b21d224ff3a84
Энди Лабрадуду <ford.mustard@yandex.ru>
2026-08-27 14:02:39 +0300

    ORD-1 initialize sping boot project

---
+++ src/test/java/io/github/kansasprobably/orderflow/OrderflowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kansasprobably.orderflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
