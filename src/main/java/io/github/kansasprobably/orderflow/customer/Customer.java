cc15f52e89f0aa14670af9e844f68a12d5749732
KansasProbably <160323965+KansasProbably@users.noreply.github.com>
2026-09-02 02:37:02 +0300

    ORD-2  add customer persistence model (#2)
    * ORD-2  add customer persistence model
    
    * ORD-2 changes have been made in accordance with the requierments
    
    * ORD-2 last fixes

---
+++ src/main/java/io/github/kansasprobably/orderflow/customer/Customer.java
@@ -0,0 +1,61 @@
+package io.github.kansasprobably.orderflow.customer;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Table(
+        name = "customers",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_customer_email",
+                        columnNames = "email"
+                )
+        })
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+
+
+    @Column(name = "email", nullable = false, length = 255)
+    private String email;
+
+    @Column(name = "phone", length = 20)
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "customer_status", nullable = false, length = 20)
+    private CustomerStatus status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+
+
+
+    public Customer(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.status = CustomerStatus.ACTIVE;
+    }
+}
