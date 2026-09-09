df6e8f45de5cc4c4446aa1c311d17f6f9d17a5e5
KansasProbably <lilmodou14@gmail.com>
2026-09-09 19:05:02 +0300

    super final fixes

--- src/main/java/io/github/kansasprobably/orderflow/order/Order.java
+++ src/main/java/io/github/kansasprobably/orderflow/order/Order.java
@@ -7,7 +7,6 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.aspectj.weaver.ast.Or;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
